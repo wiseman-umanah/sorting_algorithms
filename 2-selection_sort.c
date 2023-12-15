d0e2f3532aff913a2a6f51be2f32ee741d5c8f01
wiseman-umanah <wisemanumanah@gmail.com>
2023-12-15 01:54:31 +0100

    Fixed betty issue on code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,13 @@
 #include "sort.h"
 #include <stdio.h>
 
+/**
+* selection_sort - This is a code shows how
+* selection sort algorithm works
+* @array: The array to implement on
+* @size: The size of the array
+*/
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
@@ -9,7 +16,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for ( j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
